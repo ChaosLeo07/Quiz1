b8a31aec26f7da1e7d4fce38140bb5df527af229
ChaosLeo07 <58913672+ChaosLeo07@users.noreply.github.com>
2021-02-04 09:59:58 +0100

    Add Main File

---
+++ quiz1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace quiz1
+{
+    class Quiz1
+    {
+        static void Main(string[] args)
+        {
+            string p1 = "point";
+            string p2 = "points";
+            Console.WriteLine("what is 9+10");
+            int a = Convert.ToInt16(Console.ReadLine());
+            int b = 0;
+            if (a == 19)
+            {
+                b += 1;
+                Console.WriteLine("good job now u have " + b + " " + p1);
+            }
+            else
+            {
+                if (a != 19)
+                    b -= 1;
+                Console.WriteLine("nah now u have " + b + " " + p1);
+            }
+
+            Console.WriteLine("what is 6+5");
+            int c = Convert.ToInt16(Console.ReadLine());
+
+            if (c == 11)
+            {
+                b += 1;
+                if (b > 1)
+                {
+                    Console.WriteLine("good job now u have " + b + " " + p2);
+                }
+                else
+                    if (b == 1 || b == 0 || b == -1)
+                {
+                    Console.WriteLine("good job now u have " + b + " " + p1);
+
+                }
+            }
+            if (c != 11)
+            {
+                b -= 1;
+                if (b > 1)
+                {
+                    Console.WriteLine("nah now u have " + b + " " + p2);
+                }
+                else
+                    if (b == 1 || b == 0 || b == -1)
+                {
+                    Console.WriteLine("nah now u have " + b + " " + p1);
+                }
+                else
+                        if (b < -1)
+                {
+                    Console.WriteLine("nah now u have " + b + " " + p2);
+                }
+
+            }
+            Console.WriteLine("Press any Key to Exit...");
+            Console.ReadKey();
+
+        }
+    }
+
+}
