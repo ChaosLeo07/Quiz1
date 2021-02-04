bece2010d671810ec424ab474a21f02ca8d1b7cb
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-02-04 10:43:33 +0100

    fix typo

--- quiz1.cs
+++ quiz1.cs
@@ -8,22 +8,22 @@ namespace quiz1
         {
             string p1 = "point";
             string p2 = "points";
-            Console.WriteLine("what is 9+10");
+            Console.WriteLine("What is 9+10?");
             int a = Convert.ToInt16(Console.ReadLine());
             int b = 0;
             if (a == 19)
             {
                 b += 1;
-                Console.WriteLine("good job now u have " + b + " " + p1);
+                Console.WriteLine("Good job now u have " + b + " " + p1 + "!");
             }
             else
             {
                 if (a != 19)
                     b -= 1;
-                Console.WriteLine("nah now u have " + b + " " + p1);
+                Console.WriteLine("Nah now u have " + b + " " + p1 + "!");
             }
 
-            Console.WriteLine("what is 6+5");
+            Console.WriteLine("What is 6+5?");
             int c = Convert.ToInt16(Console.ReadLine());
 
             if (c == 11)
@@ -31,12 +31,12 @@ namespace quiz1
                 b += 1;
                 if (b > 1)
                 {
-                    Console.WriteLine("good job now u have " + b + " " + p2);
+                    Console.WriteLine("Good job now u have " + b + " " + p2 + "!");
                 }
                 else
                     if (b == 1 || b == 0 || b == -1)
                 {
-                    Console.WriteLine("good job now u have " + b + " " + p1);
+                    Console.WriteLine("Good job now u have " + b + " " + p1 + "!");
 
                 }
             }
@@ -45,21 +45,21 @@ namespace quiz1
                 b -= 1;
                 if (b > 1)
                 {
-                    Console.WriteLine("nah now u have " + b + " " + p2);
+                    Console.WriteLine("Nah now u have " + b + " " + p2 + "!");
                 }
                 else
                     if (b == 1 || b == 0 || b == -1)
                 {
-                    Console.WriteLine("nah now u have " + b + " " + p1);
+                    Console.WriteLine("Nah now u have " + b + " " + p1 + "!");
                 }
                 else
                         if (b < -1)
                 {
-                    Console.WriteLine("nah now u have " + b + " " + p2);
+                    Console.WriteLine("Nah now u have " + b + " " + p2 + "!");
                 }
 
             }
-            Console.WriteLine("Press any Key to Exit...");
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
         }
